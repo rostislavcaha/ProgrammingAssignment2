616824c72ab0524801b6c3ccb2ceb336be4703a6
rostislavcaha <rostislav.caha@axa.cz>
2020-07-07 22:37:19 +0200

    defined 2 functions: makeCacheMatrix and cacheSolve and shows usage of them

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates special vector of 4 functions - set the value of matrix, get the value of matrix, use stored inverse matrix, calculate inverse and store
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL # cash to store inverse matrix
+  set <- function(y) {  #stores base matrix and clears inverse matrix (when changed matrix, then inverse should be recalculated)
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x # get matrix data for calculation inverse
+  setinverse <- function(inverse) m <<- inverse #store inverse matrix
+  getinverse <- function() m #get inverse matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve return stored inverse matrix in case it was calculated previosly. 
+## In other case it calculates inverse, stores it and returns
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
+
+x <- makeCacheMatrix(matrix(c(2,0,0,2),2,2)) #diagonal matrix with 2'a on diagonal.
+cacheSolve(x) #full calculation
+cacheSolve(x) #get cached data
+
